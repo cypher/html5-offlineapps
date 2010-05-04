3d139c538fc5cd4a672593c984030308c7ae5bbe
Markus Prinz <markus.prinz@nuclearsquid.com>
2010-05-04 22:16:50 +0200

    Minor tweaks

--- notepad.rb
+++ notepad.rb
@@ -2,12 +2,9 @@ require 'rack'
 require 'sinatra'
 
 configure do
-  Rack::Mime::MIME_TYPES[".manifest"] = "text/cache-manifest"
+  Rack::Mime::MIME_TYPES['.manifest'] = 'text/cache-manifest'
 end
 
 get '/' do
-end
-
-get '/notepad.manifest' do
-  content_type ''
+  redirect '/notepad.html'
 end
